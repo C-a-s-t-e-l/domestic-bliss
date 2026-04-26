5a17a333d62ae7f665aba5cbef034b8003d5797c
Cas <ezzyllena@gmail.com>
2026-04-27 02:41:13 +0800

    ic imgs need work - test netlify

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,23 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Domestic Bliss Cleaning Services",
+    email: "info@domesticblissdurham.co.uk",
+    phoneForTel: "0191-555-0123",
+    phoneFormatted: "0191 555 0123",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        lineOne: "12 Durham Business Park",
+        lineTwo: "Front Street",
+        city: "Durham",
+        state: "County Durham",
+        zip: "DH1 1TH",
+        country: "UK",
+        mapLink: "https://maps.app.goo.gl/realistic-link-here",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/domesticbliss",
+        instagram: "https://www.instagram.com/domesticbliss",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.domesticblisscleaning.co.uk",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
